b3b0748024a83e9612f9629926cd9b00cf2aafa8
Satheesh C <satheesancchandran@gmail.com>
2020-08-28 07:21:09 +0530

    Added Base component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Base from './base';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const expressions = [
+  /^[0-1\b]+$/,
+  /^[0-2\b]+$/,
+  /^[0-3\b]+$/,
+  /^[0-4\b]+$/,
+  /^[0-5\b]+$/,
+  /^[0-6\b]+$/,
+  /^[0-7\b]+$/,
+  /^[0-8\b]+$/,
+  /^[0-9\b]+$/
+];
+
+class Master extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { number: 0 };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  async onChange(number) {
+    await this.setState({ number });
+    console.log(number, this.state.number);
+  }
+
+  render() {
+    const baseComponents = expressions.map((exp, index) => {
+      const baseValue = index + 2;
+      const number = this.state.number;
+      const value = this.state.number ? number.toString(baseValue) : '';
+      return (
+        <Base
+          key={index}
+          base={baseValue}
+          expression={exp}
+          onChange={this.onChange}
+          value={value}
+        />
+      );
+    });
+    return <div>{baseComponents}</div>;
+  }
 }
 
-export default App;
+// parseInt('1001', 2);
+// const decimalToBinary = (start, end, base) => {
+//   const range = (start, end) =>
+//     Array.from(new Array(end - start), (_, index) => start + index);
+//   return range(start, end).map(num => num.toString(base));
+// };
+
+// decimalToBinary(0, 20, 2);
+
+export default Master;
---
+++ src/base.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Base extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: this.props.value };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  async onChange(event) {
+    const value = event.target.value;
+    if (this.props.expression.test(value)) {
+      await this.setState({ value });
+      this.props.onChange(parseInt(this.state.value, this.props.base));
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <label>Base {this.props.base} : </label>
+        <input
+          type='text'
+          value={this.state.value}
+          onChange={this.onChange}
+        ></input>
+      </div>
+    );
+  }
+}
+
+export default Base;
